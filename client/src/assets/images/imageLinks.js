7de12a343418e4ab8c06e539920136c5eafd1ff8
AFSAL333 <afsalafs8008@gmail.com>
2024-08-10 20:55:00 +0530

    updated

--- client/src/assets/images/imageLinks.js
+++ client/src/assets/images/imageLinks.js
@@ -1,6 +1,6 @@
 const imageLinks = {
   auth: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/auth.jpg',
-  logo: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/logo.png',
+  logo: 'https://res.cloudinary.com/cloudverse/image/upload/v1721893242/CODSIFY/codsify.png',
   profile: 'https://res.cloudinary.com/cloudverse/image/upload/v1702630432/CODEVERSE/profile.png',
   vertical: 'https://res.cloudinary.com/cloudverse/image/upload/v1702631876/CODEVERSE/vertical.webp',
   horizontal: 'https://res.cloudinary.com/cloudverse/image/upload/v1703225521/CODEVERSE/horizontal.png',
